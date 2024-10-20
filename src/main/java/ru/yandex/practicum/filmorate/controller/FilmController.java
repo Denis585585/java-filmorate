c052fa5ce9f87ec512f2082944e9c5add993c8df
Denis585585 <Vegan585@yandex.ru>
2024-10-20 14:50:05 +0300

    ТЗ_10.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,61 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@Slf4j
+@RequestMapping("/films")
 public class FilmController {
+
+    private int nextId = 1;
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    //Получаем фильмы
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        log.info("Добавлен фильм: {}", film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    private int getNextId() {
+        return nextId++;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (!films.containsKey(newFilm.getId())) {
+            throw new ValidationException("Фильм по такому id не найден");
+        }
+        Film film = films.get(newFilm.getId());
+        if (newFilm.getName() != null && !newFilm.getName().isBlank()) {
+            film.setName(newFilm.getName());
+        }
+        if (newFilm.getDescription() != null && !newFilm.getDescription().isBlank()) {
+            film.setDescription(newFilm.getDescription());
+        }
+        if (newFilm.getReleaseDate() != null) {
+            film.setReleaseDate(newFilm.getReleaseDate());
+        }
+        if (newFilm.getDuration() >= 0) {
+            film.setDuration(newFilm.getDuration());
+        }
+        log.info("Фильм обновлен: {}", film);
+        films.put(film.getId(), film);
+        return film;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    private int nextId = 1;
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        user.setId(getNextId());
+        log.info("Добавлен новый пользователь: {}", user);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private int getNextId() {
+        return nextId++;
+    }
+
+    @PutMapping
+    public User updateUser(@Valid @RequestBody User newUser) {
+        if (!users.containsKey(newUser.getId())) {
+            throw new ValidationException("User with this id not found");
+        }
+        User user = users.get(newUser.getId());
+        if (newUser.getEmail() != null && !newUser.getEmail().isBlank()
+                && newUser.getEmail().matches("^[\\w-.]+@[\\w-]+(\\.[\\w-]+)*\\.[a-z]{2,}$")) {
+            user.setEmail(newUser.getEmail());
+        }
+        if (newUser.getLogin() != null && !newUser.getLogin().isBlank()) {
+            user.setLogin(newUser.getLogin());
+        }
+        if (newUser.getName() != null && !newUser.getName().isBlank()) {
+            user.setName(newUser.getName());
+        }
+        if (newUser.getBirthday() != null) {
+            user.setBirthday(newUser.getBirthday());
+        }
+        log.info("User updated: {}", user);
+        users.put(newUser.getId(), user);
+        return user;
+    }
+
+
+}
